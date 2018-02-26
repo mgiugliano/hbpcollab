7878e6cd9182591c615dabc06857d991dd212875
Cverbist <christophe.verbist@uantwerpen.be>
2018-02-26 17:03:34 +0100

    removed Cvode

--- BBPHelper.py
+++ BBPHelper.py
@@ -12,7 +12,7 @@ import random
 # the mean.
 #
 def load_model():
-    h.cvode.cache_efficient(0)
+    #h.cvode.cache_efficient(0)
     h.load_file("stdrun.hoc")
     h.load_file("import3d.hoc")
     h.load_file("constants.hoc")
